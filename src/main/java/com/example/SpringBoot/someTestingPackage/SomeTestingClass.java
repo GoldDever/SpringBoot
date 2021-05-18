9ff2a5b8b1667360e74bd9d17d49768d438a9c51
GoldDever <69146353+GoldDever@users.noreply.github.com>
2021-05-19 01:48:00 +0300

    deleting

--- src/main/java/com/example/SpringBoot/someTestingPackage/SomeTestingClass.java
+++ src/main/java/com/example/SpringBoot/someTestingPackage/SomeTestingClass.java
@@ -2,7 +2,6 @@ package com.example.SpringBoot.someTestingPackage;
 
 public class SomeTestingClass {
     public static void main(String[] args) {
-        System.out.println("Some new changes");
         System.out.println("Adding additional changes");
     }
 }
