a3810ebcda7389a6de953eab6d357b684ec98cd5
GoldDever <69146353+GoldDever@users.noreply.github.com>
2021-05-19 01:57:29 +0300

    deleting sout

--- src/main/java/com/example/SpringBoot/someTestingPackage/SomeTestingClass.java
+++ src/main/java/com/example/SpringBoot/someTestingPackage/SomeTestingClass.java
@@ -2,6 +2,5 @@ package com.example.SpringBoot.someTestingPackage;
 
 public class SomeTestingClass {
     public static void main(String[] args) {
-        System.out.println("Adding additional changes");
     }
 }
