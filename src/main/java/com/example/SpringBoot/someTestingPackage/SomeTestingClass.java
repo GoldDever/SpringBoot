990947c28075b7acddfe0b4a5eac25ccdf4e6aed
GoldDever <69146353+GoldDever@users.noreply.github.com>
2021-05-19 01:59:36 +0300

    Hello my friend

--- src/main/java/com/example/SpringBoot/someTestingPackage/SomeTestingClass.java
+++ src/main/java/com/example/SpringBoot/someTestingPackage/SomeTestingClass.java
@@ -2,5 +2,6 @@ package com.example.SpringBoot.someTestingPackage;
 
 public class SomeTestingClass {
     public static void main(String[] args) {
+        System.out.println("Hello my friend!");
     }
 }
