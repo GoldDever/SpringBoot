69a6b62606c101bedb18cb2ea01e2578377dece7
GoldDever <69146353+GoldDever@users.noreply.github.com>
2022-02-14 21:23:05 +0600

    Ad id

--- src/main/java/com/example/SpringBoot/amigo/ChecClass.java
+++ src/main/java/com/example/SpringBoot/amigo/ChecClass.java
@@ -5,6 +5,8 @@ import com.example.SpringBoot.amigo.test.AmigoInterface;
 import java.time.ZonedDateTime;
 
 public class ChecClass {
+    private Long Id;
+    private ZonedDateTime createdAt;
     private AmigoInterface someClass;
     private String firstName;
     private String lastName;
