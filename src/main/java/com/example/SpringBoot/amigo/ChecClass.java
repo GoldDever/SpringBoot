d5204bf8bd1d82c9ee11153315bac8c62019a9b3
GoldDever <69146353+GoldDever@users.noreply.github.com>
2022-02-14 19:50:40 +0600

    add AmigoInterface field

--- src/main/java/com/example/SpringBoot/amigo/ChecClass.java
+++ src/main/java/com/example/SpringBoot/amigo/ChecClass.java
@@ -1,7 +1,10 @@
 package com.example.SpringBoot.amigo;
 
+import com.example.SpringBoot.amigo.test.AmigoInterface;
+
 import java.time.ZonedDateTime;
 
 public class ChecClass {
     private ZonedDateTime createdAt;
+    private AmigoInterface someClass;
 }
