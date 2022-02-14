f3e1d1505c11c61835223ee94b1e8fd05cbccf5e
GoldDever <69146353+GoldDever@users.noreply.github.com>
2022-02-14 17:48:19 +0600

    Add setGender()

--- src/main/java/com/example/SpringBoot/amigo/AmigoClass.java
+++ src/main/java/com/example/SpringBoot/amigo/AmigoClass.java
@@ -4,4 +4,20 @@ public class AmigoClass {
     private String name;
     private int age;
     private boolean gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
 }
