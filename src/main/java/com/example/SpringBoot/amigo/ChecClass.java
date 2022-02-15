bfe759aa82c910000578715de6d535b3897c51c4
GoldDever <69146353+GoldDever@users.noreply.github.com>
2022-02-15 10:44:41 +0600

    Remove createdAt

--- src/main/java/com/example/SpringBoot/amigo/ChecClass.java
+++ src/main/java/com/example/SpringBoot/amigo/ChecClass.java
@@ -5,7 +5,6 @@ import com.example.SpringBoot.amigo.test.AmigoInterface;
 import java.time.ZonedDateTime;
 
 public class ChecClass {
-    private ZonedDateTime createdAt;
     private AmigoInterface someClass;
     private String firstName;
     private String lastName;
